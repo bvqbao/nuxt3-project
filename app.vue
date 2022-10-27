f94f49f2b633eae6a0ebbdd56fd30b0bcfff5e9e
Bao Bui <bvqbao@gmail.com>
2022-10-27 17:17:40 +0700

    Remove layouts

--- app.vue
+++ app.vue
@@ -1,5 +1,7 @@
 <template>
-	<NuxtLayout>
+	<div
+		class="flex flex-col items-center w-full h-full min-h-screen p-12 bg-gray-100"
+	>
 		<NuxtPage />
-	</NuxtLayout>
+	</div>
 </template>
--- layouts/default.vue
+++
@@ -1,7 +0,0 @@
-<template>
-	<div
-		class="flex flex-col items-center w-full h-full min-h-screen p-12 bg-gray-100"
-	>
-		<slot />
-	</div>
-</template>
