b6d79a536a298e715bf5962cc2852bc50ff22d86
Bao Bui <bvqbao@gmail.com>
2023-07-25 22:20:31 +0700

    Add nuxt-content module config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,4 +2,22 @@
 export default defineNuxtConfig({
 	devtools: { enabled: true },
 	modules: ['@nuxtjs/tailwindcss', '@nuxt/content'],
+	content: {
+		// https://content.nuxtjs.org/api/configuration
+		highlight: {
+			theme: 'nord',
+			preload: [
+				'ts',
+				'js',
+				'json',
+				'css',
+				'vue',
+				'html',
+				'vue-html',
+				'bash',
+				'dart',
+				'php',
+			],
+		},
+	},
 });
