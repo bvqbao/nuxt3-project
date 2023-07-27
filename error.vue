5356b7dfc3056f4b30c8ce50101e209a0f503ff4
Bao Bui <bvqbao@gmail.com>
2023-07-28 02:51:36 +0700

    Use tailwindcss prettier plugin

--- error.vue
+++ error.vue
@@ -11,7 +11,7 @@ const handleError = () => clearError({ redirect: '/' });
 
 <template>
 	<main
-		class="prose max-w-none flex h-full min-h-screen w-full flex-col items-center bg-gray-100 p-12"
+		class="prose flex h-full min-h-screen w-full max-w-none flex-col items-center bg-gray-100 p-12"
 	>
 		<h1>{{ error.message }}</h1>
 		<button @click="handleError">Go back home</button>
